19a87c9a74f577a226c6bf05d3417ef26bec3e1a
tanatana <aiueo2003haru@gmail.com>
2026-01-20 23:19:35 +0900

    hudを設定

---
+++ Source/Golf/GolfHUDBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "GolfHUDBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLF_API AGolfHUDBase : public AHUD
+{
+	GENERATED_BODY()
+	
+};
